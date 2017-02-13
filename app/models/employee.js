ff96f45024546603a53f42d4839ad9ff9f6e414d
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 21:05:06 -0200

    Início desenvolvimento do modelo: Employee no arquivo: employee.js(models)

---
+++ app/models/employee.js
@@ -0,0 +1,7 @@
+/**
+ * Arquivo: models/employee.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelo modelo do 'Livro' para realizar a conexão com a base 
+ *  de dados via Moongose.
+ * Data: 13/02/2017
+ */
