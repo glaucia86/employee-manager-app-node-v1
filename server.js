e22adec9d1f7f5ac218a25e52c6ace1882ad2ae1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 21:40:25 -0200

    Acerto no arquivo - server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 
 var express = require('express');
 var app = express();
-var mongoose = require(mongoose);
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var port = 8000;
 var employee = require('./app/routes/employee');
