d59f681dfb7d5d6248ba52c294135c5c929c9dde
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 21:20:10 -0200

    Início desenvolvimento das rotas da app.

---
+++ app/routes/employee.js
@@ -0,0 +1,11 @@
+/*
+ * Arquivo: routes/employee.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelas rotas das APIS
+ * Data: 13/02/2017
+ */
+
+var mongoose = require('mongoose');
+var Employee = require('../models/employee');
+
+/* 1) Método: Selecionar Livros (acessar em: GET http://localhost:8000/livro */
